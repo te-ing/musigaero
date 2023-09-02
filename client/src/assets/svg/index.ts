59c0f4f18efe5952bb379dbdfb36c7fedbcd515b
te-ing <hi2177@naver.com>
2023-09-02 22:28:47 +0900

    feat: logo svg 추가

---
+++ client/src/assets/svg/index.ts
@@ -0,0 +1,6 @@
+export { ReactComponent as LogoColorIcon } from './logo_color.svg';
+export { ReactComponent as LogoWhiteIcon } from './logo_white.svg';
+export { ReactComponent as LogoEnglishColorIcon } from './logo_english_color.svg';
+export { ReactComponent as LogoEnglishWhiteIcon } from './logo_english_white.svg';
+export { ReactComponent as LogoKoreanColorIcon } from './logo_korean_color.svg';
+export { ReactComponent as LogoKoreanWhiteIcon } from './logo_korean_white.svg';
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import * as path from 'path';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   build: {},
   resolve: {
     alias: {
