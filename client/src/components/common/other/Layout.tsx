37ab0c27eecdc98b4a5e975027a321c50900ff7a
te-ing <hi2177@naver.com>
2023-09-17 11:43:19 +0900

    feat: 홈 UI 임시구현

--- client/src/components/common/other/Layout.tsx
+++ client/src/components/common/other/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
-    <div className="h-screen w-scree">
+    <div className="h-screen w-screen">
       <Outlet />
     </div>
   );
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,17 +1,32 @@
 import useModal from '@/hooks/useModal';
 import { Link } from 'react-router-dom';
+import { LogoKoreanWhiteIcon } from '@/assets/svg';
+import { sx } from '@/constants/styles';
 
 const Home = () => {
   const { showToast } = useModal();
   return (
-    <div>
-      <Link to={'/login'}>로그인 페이지</Link>
-      <Link to={'/register'}>회원가입 페이지</Link>
-      <Link to={'/post'}>목록 페이지</Link>
-      <Link to={'/post/1'}>상품 페이지</Link>
-      <button onClick={() => showToast('토스트 클릭')}>Toast</button>
+    <div className={sxHome.container}>
+      <LogoKoreanWhiteIcon />
+      <p className="text-white w-80 mt-14 mb-10">안녕하세요 {'닉네임'}님 🐶</p>
+      <div
+        className={`${sx.flexCenter} flex-col w-full gap-6 [&>a]:bg-primary [&>a]:w-80 [&>a]:h-14 [&>a]:rounded-2xl [&>a]:flex [&>a]:justify-center [&>a]:items-center text-lg`}
+      >
+        <Link to={'/login'}>로그인 페이지</Link>
+        <Link to={'/register'}>회원가입 페이지</Link>
+        <Link to={'/post'}>목록 페이지</Link>
+        <Link to={'/post/1'}>상품 페이지</Link>
+        <button onClick={() => showToast('토스트 클릭')}>Toast</button>
+      </div>
+      <div className="font-light text-xs mt-28">
+        <Link to={'/user'}>내 정보 수정</Link> | <Link to={'/user'}>회원탈퇴</Link>
+      </div>
     </div>
   );
 };
 
 export default Home;
+
+const sxHome = {
+  container: `${sx.flexCenter} py-20 flex-col w-full text-white bg-petPhotos bg-cover `,
+};
