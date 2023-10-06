63d8919b1a759f1b7813113a826f9159a96b65ae
te-ing <hi2177@naver.com>
2023-10-06 16:13:01 +0900

    chore: svg 폴더 구조 변경

--- client/src/assets/svg/index.ts
+++ client/src/assets/svg/index.ts
@@ -1,6 +1,12 @@
-export { ReactComponent as LogoColorIcon } from './logo_color.svg';
-export { ReactComponent as LogoWhiteIcon } from './logo_white.svg';
-export { ReactComponent as LogoEnglishColorIcon } from './logo_english_color.svg';
-export { ReactComponent as LogoEnglishWhiteIcon } from './logo_english_white.svg';
-export { ReactComponent as LogoKoreanColorIcon } from './logo_korean_color.svg';
-export { ReactComponent as LogoKoreanWhiteIcon } from './logo_korean_white.svg';
+// icon
+export { ReactComponent as LeftBracketIcon } from './icon/left_bracket.svg';
+export { ReactComponent as MoreIcon } from './icon/more.svg';
+export { ReactComponent as PencilIcon } from './icon/pencil.svg';
+
+// logo
+export { ReactComponent as LogoColorIcon } from './logo/logo_color.svg';
+export { ReactComponent as LogoWhiteIcon } from './logo/logo_white.svg';
+export { ReactComponent as LogoEnglishColorIcon } from './logo/logo_english_color.svg';
+export { ReactComponent as LogoEnglishWhiteIcon } from './logo/logo_english_white.svg';
+export { ReactComponent as LogoKoreanColorIcon } from './logo/logo_korean_color.svg';
+export { ReactComponent as LogoKoreanWhiteIcon } from './logo/logo_korean_white.svg';
