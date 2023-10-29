99d6fdc1127314eb927b7040ff3bbff20884f77f
te-ing <hi2177@naver.com>
2023-10-29 10:49:58 +0900

    feat: 이미지 originalname 한글 깨짐 현상 수정

--- server/src/upload/upload.service.ts
+++ server/src/upload/upload.service.ts
@@ -7,12 +7,13 @@ export class UploadService {
   async uploadImage(files: Express.Multer.File[]) {
     const result = [];
     files.forEach((file) => {
-      fs.writeFile(`./static/images/${ulid()}-${file.originalname}`, file.buffer, 'binary', (error) => {
+      const originalname = Buffer.from(file.originalname, 'latin1').toString('utf8');
+      fs.writeFile(`./static/images/${ulid()}-${originalname}`, file.buffer, 'binary', (error) => {
         if (error) {
           throw new HttpException({ message: '저장 중 알 수 없는 오류가 발생했습니다.', error }, 500);
         }
       });
-      result.push(`${ulid()}-${file.originalname}`);
+      result.push(`${ulid()}-${originalname}`);
     });
     return result;
   }
