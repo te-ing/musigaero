997543ee663bc5f77445da3674bf9b1a1a8e3ecf
te-ing <hi2177@naver.com>
2023-11-07 21:58:44 +0900

    fix: 업로드 시 ulid()가 새로 생성되어 반환되던 문제 수정

--- server/src/upload/upload.service.ts
+++ server/src/upload/upload.service.ts
@@ -7,13 +7,15 @@ export class UploadService {
   async uploadImage(files: Express.Multer.File[]) {
     const result = [];
     files.forEach((file) => {
-      const originalname = Buffer.from(file.originalname, 'latin1').toString('utf8').slice(0, 10);
-      fs.writeFile(`./static/images/${ulid()}-${originalname}`, file.buffer, 'binary', (error) => {
+      const originalname =
+        Buffer.from(file.originalname, 'latin1').toString('utf8').slice(0, 10) + file.mimetype.replace('/', '.');
+      const savename = `${ulid()}-${originalname}`;
+      fs.writeFile(`./static/images/${savename}`, file.buffer, 'binary', (error) => {
         if (error) {
           throw new HttpException({ message: '저장 중 알 수 없는 오류가 발생했습니다.', error }, 500);
         }
       });
-      result.push(`${ulid()}-${originalname}`);
+      result.push(savename);
     });
     return result;
   }
