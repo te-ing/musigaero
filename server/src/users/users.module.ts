19a3174f746a9054e5c3d7dfa30aeaa74764a53a
te-ing <hi2177@naver.com>
2023-12-10 12:30:55 +0900

    feat: jwt token 유효기간 수정

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -12,7 +12,7 @@ import { JwtStrategy } from './jwt.strategy';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '6h' },
+      signOptions: { expiresIn: '2 days' },
     }),
   ],
   controllers: [UsersController],
