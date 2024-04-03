03bfd382a144736d0efccb7e0cc1c5564ad3992a
te-ing <hi2177@naver.com>
2024-04-04 00:03:40 +0900

    feat: https 적용을 위한 docker, server app option 수정

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const appOptions = { cors: true };
+  const httpsOptions = {
+    key: fs.readFileSync(process.env.SERVER_PRIVKEY_LOCATION),
+    cert: fs.readFileSync(process.env.SERVER_FULLCHAIN_LOCATION),
+  };
+  const appOptions = { cors: true, httpsOptions };
   const app = await NestFactory.create<NestExpressApplication>(AppModule, appOptions);
   app.useStaticAssets('static/images', { prefix: '/static/images' });
   await app.listen(process.env.SERVER_API_PORT);
