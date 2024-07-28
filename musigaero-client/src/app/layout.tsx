a2ad5f15e77d5dc9d51f14e9f35ee0f553a6dd3c
te-ing <hi2177@naver.com>
2024-07-28 15:58:18 +0900

    feat: svg 파일 추가 및 @svgr/webpack 설치

--- musigaero-client/src/app/layout.tsx
+++ musigaero-client/src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} flex justify-center bg-slate-200`}>
+        <div className="w-full max-w-xl bg-white" aria-label="모바일 wrapper">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
